1d1fe536b15a64ddf0e53e4219b337649f9a779b
DjLiso <djliso.rap@gmail.com>
2024-05-03 21:00:14 -0300

    Tentativa de Saque

--- src/exemplos/TesteConta.java
+++ src/exemplos/TesteConta.java
@@ -5,6 +5,14 @@ public class TesteConta {
         Conta c = new Conta();
         c.depositar(100);
         c.depositar(50);
+        boolean saqueEfetuado = c.sacar(300);
+        if (saqueEfetuado){
+            System.out.println("Saque Realizado");
+        }else{
+            System.err.println("ERRO");
+        }
         System.out.println("Saldo: " + c.saldo);
+       
+              
     }
 }
