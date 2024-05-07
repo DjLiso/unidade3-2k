c67a37ecd280d38e4d8f180a826872a4e9a641c6
DjLiso <167827193+DjLiso@users.noreply.github.com>
2024-05-06 21:58:01 -0300

    Encapsulamento

--- src/exemplos/TesteConta.java
+++ src/exemplos/TesteConta.java
@@ -4,13 +4,20 @@ public class TesteConta {
     public static void main(String[] args) {
         Conta c = new Conta();
         c.depositar(100);
-        c.depositar(50);
-        boolean saqueEfetuado = c.sacar(300);
+        c.depositar(100);
+        c.setNumero(666);
+        c.setNomeTitular("Thiago");
+      
+        boolean saqueEfetuado = c.sacar(29);
         if (saqueEfetuado) {
             System.out.println("Saque efetuado com sucesso!");
         } else {
             System.err.println("ERRO AO SACAR!!!");
         }
-        System.out.println("Saldo: " + c.saldo);
+      ///  System.out.println("Nome do Titular: "+c.getNomeTitular());
+      ///  System.err.println("Número: "+c.getNumero());
+      ///  System.out.println("Saldo: " + c.getSaldo());
+        
+        System.out.println(c);
     }
 }
